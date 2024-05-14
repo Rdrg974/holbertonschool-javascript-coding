63d913c7727b2e5f9c2aa9e9dcda25a11d247408
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-05-14 02:12:25 +0200

    Star wars movie title

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* prints the title of a Star Wars movie 
+where the episode number matches a given integer.
+*/
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
