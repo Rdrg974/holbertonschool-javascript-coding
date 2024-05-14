1972f926607696c5cf01c05c6b7a1362cf6bfbd1
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-05-14 02:10:02 +0200

    Status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* prints the status code of a GET request. */
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, response) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
