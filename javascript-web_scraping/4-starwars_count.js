dd04d520e5e4e9df58e9a6f32ebb06ab2d02ecfd
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-05-14 02:35:51 +0200

    Fix code

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,17 +5,16 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (err, response, body) => {
-    if (err) {
-        console.log(err);
-    }
-    let count = 0;
-    const films = JSON.parse(body).results;
-    for (const film of films) {
-        for (const character of film.characters) {
-            if (character.includes('18')) {
-                count++;
-            }
-        }
-    }
-    console.log(count);
+  if (err) {
+    console.log(err);
+  }
+  let count = 0;
+  const films = JSON.parse(body).results;
+  for (const film of films) {
+    for (const character of film.characters) {
+      if (character.includes('18')) {
+        count++;
+      }
+    }}
+  console.log(count);
 });
